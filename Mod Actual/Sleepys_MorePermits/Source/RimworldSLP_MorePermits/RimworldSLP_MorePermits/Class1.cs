f1f406ca62b4c2557b191df298ad22787d73b584
sleepy068 <58592380+sleepy068@users.noreply.github.com>
2021-11-02 02:12:54 +1100

    sync

--- Mod Actual/Sleepys_MorePermits/Source/RimworldSLP_MorePermits/RimworldSLP_MorePermits/Class1.cs
+++ Mod Actual/Sleepys_MorePermits/Source/RimworldSLP_MorePermits/RimworldSLP_MorePermits/Class1.cs
@@ -144,13 +144,106 @@ namespace RimworldSLP_MorePermits
 		}
 	}
 
+	public class RoyalTitlePermitWorker_SLP_CallStellicGuardMelee : RoyalTitlePermitWorker
+	{
+		public override IEnumerable<FloatMenuOption> GetRoyalAidOptions(Map map, Pawn pawn, Faction faction)
+		{
+			string t;
+			if (this.AidDisabled(map, pawn, faction, out t))
+			{
+				yield return new FloatMenuOption(this.def.LabelCap + ": " + t, null, MenuOptionPriority.Default, null, null, 0f, null, null, true, 0);
+				yield break;
+			}
+			Action action = null;
+			string label = this.def.LabelCap + " (" + "CommandCallStellicMGNumStellicMG".Translate(this.def.royalAid.pawnCount) + "): ";
+			bool free;
+			if (base.FillAidOption(pawn, faction, ref label, out free))
+			{
+				action = delegate ()
+				{
+					this.CallStellicGuardMelee(pawn, map, faction, free);
+				};
+			}
+			yield return new FloatMenuOption(label, action, faction.def.FactionIcon, faction.Color, MenuOptionPriority.Default, null, null, 0f, null, null, true, 0);
+			yield break;
+		}
+
+		private void CallStellicGuardMelee(Pawn pawn, Map map, Faction faction, bool free)
+		{
+			if (faction.HostileTo(Faction.OfPlayer))
+			{
+				return;
+			}
+			QuestScriptDef Permit_SLP_CallStellicGuardMelee = QuestScriptDefOfSLP.Permit_SLP_CallStellicGuardMelee;
+			Slate slate = new Slate();
+			slate.Set<Map>("map", map, false);
+			slate.Set<int>("laborersCount", this.def.royalAid.pawnCount, false);
+			slate.Set<Faction>("permitFaction", faction, false);
+			slate.Set<PawnKindDef>("laborersPawnKind", this.def.royalAid.pawnKindDef, false);
+			slate.Set<float>("laborersDurationDays", this.def.royalAid.aidDurationDays, false);
+			QuestUtility.GenerateQuestAndMakeAvailable(Permit_SLP_CallStellicGuardMelee, slate);
+			pawn.royalty.GetPermit(this.def, faction).Notify_Used();
+			if (!free)
+			{
+				pawn.royalty.TryRemoveFavor(faction, this.def.royalAid.favorCost);
+			}
+		}
+	}
+
+	public class RoyalTitlePermitWorker_SLP_CallStellicGuardRanged : RoyalTitlePermitWorker
+	{
+		public override IEnumerable<FloatMenuOption> GetRoyalAidOptions(Map map, Pawn pawn, Faction faction)
+		{
+			string t;
+			if (this.AidDisabled(map, pawn, faction, out t))
+			{
+				yield return new FloatMenuOption(this.def.LabelCap + ": " + t, null, MenuOptionPriority.Default, null, null, 0f, null, null, true, 0);
+				yield break;
+			}
+			Action action = null;
+			string label = this.def.LabelCap + " (" + "CommandCallStellicRGNumStellicRG".Translate(this.def.royalAid.pawnCount) + "): ";
+			bool free;
+			if (base.FillAidOption(pawn, faction, ref label, out free))
+			{
+				action = delegate ()
+				{
+					this.CallStellicGuardRanged(pawn, map, faction, free);
+				};
+			}
+			yield return new FloatMenuOption(label, action, faction.def.FactionIcon, faction.Color, MenuOptionPriority.Default, null, null, 0f, null, null, true, 0);
+			yield break;
+		}
+
+		private void CallStellicGuardRanged(Pawn pawn, Map map, Faction faction, bool free)
+		{
+			if (faction.HostileTo(Faction.OfPlayer))
+			{
+				return;
+			}
+			QuestScriptDef Permit_SLP_CallStellicGuardRanged = QuestScriptDefOfSLP.Permit_SLP_CallStellicGuardRanged;
+			Slate slate = new Slate();
+			slate.Set<Map>("map", map, false);
+			slate.Set<int>("laborersCount", this.def.royalAid.pawnCount, false);
+			slate.Set<Faction>("permitFaction", faction, false);
+			slate.Set<PawnKindDef>("laborersPawnKind", this.def.royalAid.pawnKindDef, false);
+			slate.Set<float>("laborersDurationDays", this.def.royalAid.aidDurationDays, false);
+			QuestUtility.GenerateQuestAndMakeAvailable(Permit_SLP_CallStellicGuardRanged, slate);
+			pawn.royalty.GetPermit(this.def, faction).Notify_Used();
+			if (!free)
+			{
+				pawn.royalty.TryRemoveFavor(faction, this.def.royalAid.favorCost);
+			}
+		}
+	}
+
 	[DefOf]
 	public static class QuestScriptDefOfSLP
 	{
 		public static QuestScriptDef Permit_SLP_CallTroopers;
 		public static QuestScriptDef Permit_SLP_CallJanissary;
 		public static QuestScriptDef Permit_SLP_CallCataphract;
-
+		public static QuestScriptDef Permit_SLP_CallStellicGuardMelee;
+		public static QuestScriptDef Permit_SLP_CallStellicGuardRanged;
 	}
 }
 
